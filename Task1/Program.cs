e4ba1759430110f42ef111b3349340a99879c19d
DoPiYm <135793791+DoPiYm@users.noreply.github.com>
2023-06-23 13:16:50 +0700

    task1

---
+++ Task1/Program.cs
@@ -0,0 +1,19 @@
+﻿// **Задание 1:**Напишите программу,
+// которая на вход принимает два числа и выдаёт,
+// какое число большее,.
+
+Console.Clear();
+Console.WriteLine("Введи первое число");
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+if (numberA > numberB)
+{
+Console.WriteLine("Первое число " + numberA + " Больше чем второе число " + numberB);
+}
+else
+{
+Console.WriteLine("Второе число " + numberB + " Больше чем первое число " + numberA);
+}
