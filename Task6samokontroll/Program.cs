1368d80eb0edcfe370504f344ce61afb9e82c794
DoPiYm <135793791+DoPiYm@users.noreply.github.com>
2023-06-29 09:38:49 +0700

    samokontroll-1

---
+++ Task6samokontroll/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+Console.WriteLine("Введите трехзначное число");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num >= 100 && num < 1000)
+{
+    int mid = (num % 100 - num % 10) / 10;
+    Console.WriteLine($"Вторая цифра числа {num} является {mid}");
+}
+else Console.WriteLine("Это чио не трехзначное!");
