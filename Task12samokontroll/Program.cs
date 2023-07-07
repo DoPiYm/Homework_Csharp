8c3ca290f21c881a104ff687d250ef41b88a29e6
DoPiYm <135793791+DoPiYm@users.noreply.github.com>
2023-07-07 13:57:48 +0700

    samokontr12

---
+++ Task12samokontroll/Program.cs
@@ -0,0 +1,23 @@
+﻿/*Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12*/
+
+Console.Clear();
+int GetNumber(string msg)
+{
+    Console.Write(msg);
+    int rez = Convert.ToInt32(Console.ReadLine());
+    return rez;
+}
+
+Console.Clear();
+Console.WriteLine("Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе");
+int rezult = 0;
+int a = GetNumber("Введите число: ");
+while (a > 0)
+{
+    rezult += a % 10;
+    a = a / 10;
+}
+Console.WriteLine($"Результат: {rezult}");
